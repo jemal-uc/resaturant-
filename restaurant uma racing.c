6a89d2db8bdb609a6f01dd71a7f3607d311e8e23
NicholasStp <129504859+NicholasStp@users.noreply.github.com>
2023-04-13 02:17:20 +0700

    Login add

--- restaurant uma racing.c
+++ restaurant uma racing.c
@@ -5,6 +5,12 @@
 #include <string.h>
 #include <windows.h>
 
+char user[10], pass[10];
+int login_attempt = 1, max_login_attempt = 3;
+
+void login() {
+    // Isi fungsi menu di sini
+}
 void gotoxy (int x, int y) // Tempat pemangilan - menaruh output/text sesuai kordinasi sumbu X & Y
 {
     COORD coord;
@@ -13,8 +19,8 @@ void gotoxy (int x, int y) // Tempat pemangilan - menaruh output/text sesuai kor
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
-int main(){
-char str [] = "x+";
+int main() {
+    char str [] = "x+";
         gotoxy(52, 10);
         system("color e1");
         system("color e1");
@@ -51,5 +57,43 @@ char str [] = "x+";
         Sleep (10);
     }
 }
-        system ("cls");
+   system ("cls");
+    system("color e1");
+   gotoxy(51, 10);
+    printf (" Selamat datang \n");
+    gotoxy(52, 11);
+    printf ("====================\n");
+
+
+    do {
+       gotoxy(51, 10);
+    printf (" Selamat datang \n");
+    gotoxy(52, 11);
+    printf ("====================\n");
+    gotoxy(52, 12);
+    printf("Masukan Username : ");
+    scanf("%s",&user);
+    gotoxy(52, 13);
+    printf("Masukan Password : ");
+    scanf("%s",&pass);
+
+        if ((strcmp(user, "admin") == 0 && strcmp(pass, "111") == 0)) {
+                gotoxy(52, 14);
+            printf("Akses diterima. Selamat Datang\n\n");
+            break;
+        } else {
+            gotoxy(52, 14);
+            system("cls");
+            printf("Username dan Password tidak match. ");
+            printf("Anda masih memiliki %d kesempatan login \n", max_login_attempt - login_attempt);
+            login_attempt++;
+
+        }
+    } while (login_attempt <= max_login_attempt);
+
+    if (login_attempt > max_login_attempt) {
+            gotoxy(52, 10);
+        printf("Anda sudah mencoba login sebanyak %d kali. Akun anda diblokir.\n", max_login_attempt);
+        return 0;
+    }
 }
